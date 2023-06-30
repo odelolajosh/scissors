cd9fb95725b1556b441e9cb2e68972346c5ed905
odelolajosh <oladapoodelola@gmail.com>
2023-06-30 16:24:44 +0100

    fix: some tweaks

--- apps/server/server.ts
+++ apps/server/server.ts
@@ -1,11 +1,11 @@
 import app from "./src/app";
 
 const PORT = parseInt(process.env.PORT) || 5001;
-const HOST = process.env.HOST || 'localhost';
+// const HOST = process.env.HOST || 'localhost';
 
 const startServer = async () => {
-  app.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
 }
 
